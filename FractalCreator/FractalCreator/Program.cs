fca627f92c96572805339061f73fd28efe1839d8
IvanDrv <ivander05@mail.ru>
2025-05-16 20:54:11 +0300

    Starting point

---
+++ FractalCreator/FractalCreator/Program.cs
@@ -0,0 +1,17 @@
+using FractalCreator;
+using System;
+using System.Windows.Forms;
+
+namespace FractalCreator
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
